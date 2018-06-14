5ed80dc870057bacfad0a50a147ceb8cf436d521
João Santana <joaosantana@outlook.com>
2018-06-14 16:18:34 -0300

    Modelo do banco de dados criado
    https://github.com/aspnet/EntityFramework.Docs/issues/769 descreve a edição necessária no arquivo de projeto para que Migrations funcionem como esperado.

---
+++ Recibos.Model/Migrations/20180614191354_RecibosFirstMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recibos.Model.Migrations
+{
+    public partial class RecibosFirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recibos",
+                columns: table => new
+                {
+                    ReciboId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TomadorNome = table.Column<string>(nullable: true),
+                    TomadorCpf = table.Column<string>(nullable: true),
+                    ServicoDescricao = table.Column<string>(nullable: true),
+                    ServicoValor = table.Column<string>(nullable: true),
+                    ServicoData = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recibos", x => x.ReciboId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recibos");
+        }
+    }
+}
--- Recibos.Model/Model.cs
+++ Recibos.Model/Model.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Recibos.Model
 {
-    public class Model
+    public class RecibosContext : DbContext
     {
+        public DbSet<Recibo> Recibos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Recibos.db");
+        }
+    }
+
+    public class Recibo
+    {
+        public int ReciboId { get; set; }
+        public string TomadorNome { get; set; }
+        public string TomadorCpf { get; set; }
+        public string ServicoDescricao { get; set; }
+        public string ServicoValor { get; set; }
+        public string ServicoData { get; set; }
     }
 }
